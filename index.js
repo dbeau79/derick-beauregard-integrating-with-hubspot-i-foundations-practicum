6f028bec2a7b1be6c508de44f4ab7d92cf7c3022
dbeau79 <derick@beacondigitalmarketing.com>
2025-03-18 09:57:46 -0400

    working-branch commit 3.18

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,19 +9,82 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+
+    const ebooks = 'https://api.hubspot.com/crm/v3/objects/ebooks?properties=name,ebook_description,ebook_category';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(ebooks, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'eBooks | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    //http://localhost:3000?objID=25265948432
+    const objID = req.query.objID;
+
+    const getEbook = `https://api.hubspot.com/crm/v3/objects/ebooks/${objID}?properties=name,ebook_description,ebook_category`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(getEbook, { headers });
+        const data = response.data;
+
+        //res.json(data);
+
+        res.render('update-cobj', {ebookName: data.properties.name, ebookDescription: data.properties.ebook_description});
+
+        
+    } catch(err) {
+        console.error(err);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            "ebook_description": req.body.newVal
+        }
+    }
+
+    const objID = req.query.objID;
+    const updateEbook = `https://api.hubapi.com/crm/v3/objects/ebooks/${objID}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.patch(updateEbook, update, { headers } );
+        res.redirect('back');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -68,4 +132,4 @@ app.post('/update', async (req, res) => {
 
 
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log('still Listening on http://localhost:3000'));
